8e29882817e6f2f6fc431d48f442abe3f1e8cdfd
Nitin Sapru <sapru-nitin@outlook.com>
2019-10-22 23:08:14 +0530

    Add project files.

---
+++ PollyForRetryMechanism/HttpCaller.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Polly;
+
+namespace PollyForRetryMechanism
+{
+    public static class HttpCaller
+    {
+        public static async Task<HttpResponseMessage> Get(string Uri)
+        {
+            var httpRequest = new HttpClient();
+
+            // Wrapping the http call within the executeWaitRetryOnTransientFailure method //
+            return await ExecuteWaitRetryOnTransientFailure(() => httpRequest.GetAsync(Uri));
+        }
+
+        /// <summary>
+        ///     This method is used to execute retry mechanism using RetryAsync method.
+        /// </summary>
+        /// <param name="httpResponse"></param>
+        /// <returns></returns>
+        private static Task<HttpResponseMessage> ExecuteRetryOnTransientFailure(Func<Task<HttpResponseMessage>> httpResponse)
+        {
+            return Polly.Policy
+                        .Handle<HttpRequestException>()
+                        .OrResult<HttpResponseMessage>(x=>x.StatusCode != System.Net.HttpStatusCode.OK)
+                        .RetryAsync(3,
+                                   (ex, a) => Console.WriteLine($"Retry count {a}"))
+                        .ExecuteAsync(httpResponse);
+        }
+
+        /// <summary>
+        ///     This method is used to execute retry mechanism using WaitRetryAsync which configures a certain wait 
+        ///     time before retrying the http request
+        /// </summary>
+        /// <param name="httpResponse"></param>
+        /// <returns></returns>
+        private static Task<HttpResponseMessage> ExecuteWaitRetryOnTransientFailure(Func<Task<HttpResponseMessage>> httpResponse)
+        {
+            return Polly.Policy
+                        .Handle<HttpRequestException>()
+                        .OrResult<HttpResponseMessage>(x => x.StatusCode != System.Net.HttpStatusCode.OK)
+                        .WaitAndRetryAsync(3, retryAttempt =>
+                                        TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                                      (ex, a) => Console.WriteLine($"Retrying after : {a} seconds"))
+                        .ExecuteAsync(httpResponse);
+        }
+
+
+    }
+}
---
+++ PollyForRetryMechanism/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace PollyForRetryMechanism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var validHttpUrl = "https://jsonplaceholder.typicode.com/comments";
+            var inValidHttpUrl = "https://jsonplaceholder.typicode.com/comments1";
+
+            // Calling the HttpCaller class & Get method within the class to return an HttpResponseMessage type //
+            var httpHandler = HttpCaller.Get(inValidHttpUrl).ConfigureAwait(false);
+
+            // Deserializing the http response content to an object //
+            var modelObject = JsonConvert.DeserializeObject<List<Model>>(httpHandler.GetAwaiter().GetResult()
+                                                                        .Content.ReadAsStringAsync()
+                                                                        .ConfigureAwait(false).GetAwaiter().GetResult());
+
+            foreach(var item in modelObject)
+            {
+                Console.WriteLine($"Name: {item.name}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
